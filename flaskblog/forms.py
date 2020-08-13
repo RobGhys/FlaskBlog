4b9a4fb8200a4e5234466ab046cb77036630d62f
RobGhys <robin.ghyselinck@student.unamur.be>
2020-08-13 18:26:52 +0200

    Class 8

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -44,3 +46,33 @@ class LoginForm(FlaskForm):
     # Using secure cookie, enables the user logs to be remembered
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    # Only validates input from user if i) some data are present, and ii) length is respected
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    # Only validates input if i) not empty, ii) correct address email
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+
+    def validate_username(self, username):
+        # If the user changes username, returns the user name if it exists, or returns None if it doesn't exist
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            # Raise the error if the use is None
+            if user:
+                raise ValidationError('Username already taken. Please choose a different one.')
+
+
+    def validate_email(self, email):
+        # If the user changes email, returns the user email if it exists, or returns None if it doesn't exist
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            # Raise the error if the use is None
+            if user:
+                raise ValidationError('Email already taken. Please choose a different one.')
+
