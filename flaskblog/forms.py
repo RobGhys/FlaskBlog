a25a3bf10f68ad6c1b21902e6f3963d92a209ae1
RobGhys <robin.ghyselinck@student.unamur.be>
2020-08-19 08:42:47 +0200

    class 10

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -78,3 +78,23 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
 
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request password Reset')
+
+    def validate_email(self, email):
+        # Returns the user name if it exists, or returns None if it doesn't exist
+        user = User.query.filter_by(email=email.data).first()
+        # Raise the error if the use is None
+        if user is None:
+            raise ValidationError('There is no account with that email. Please register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
